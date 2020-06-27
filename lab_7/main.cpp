64b5958dee5412f9ae05c15b4249636ebb46c29d
HomoHikka <halfideas31@gmail.com>
2020-06-27 17:16:27 +0300

    Add readarr method

--- lab_7/main.cpp
+++ lab_7/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #define N 5
 
+using namespace std;
+
+void readarr(int X[N][N])
+{
+	cout << "¬ведите элементы матрицы:\n";
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> X[i][j];
+}
+
 int main()
 {
 	setlocale(0, "");
-	int A[N][N]; int X[N];
+	int A[N][N]; int X[N], Y[N];
+
+	readarr(A);
 }
