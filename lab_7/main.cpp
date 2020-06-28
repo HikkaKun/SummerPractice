234aa008f7b53e14593cbd036056cc917f48c42b
HomoHikka <halfideas31@gmail.com>
2020-06-28 16:02:48 +0300

    Add mas_x, output methods and input choice

--- lab_7/main.cpp
+++ lab_7/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
 #define N 5
 
 using namespace std;
@@ -29,9 +30,56 @@ void filearr(int X[N][N])
 	file.close();
 }
 
+int* mas_x(int(*A)[N], int* X,
+	void (*pfunc)(int[N][N]))
+{
+	pfunc(A);
+	int k = 0, next, cur, end = 1;
+	for (int i = 0; i < N*N - 1; i++)
+	{
+		cur = A[i / 5][i % 5];
+		next = A[(i+1) / 5][(i+1) % 5];
+		switch (k)
+		{
+		case 0: if (cur < next) k = 1; if (cur > next) k = 2; break;
+		case 1: if (cur > next) end = -1; break;
+		case 2: if (cur < next) end = -1;
+		}
+	}
+	for (int i = 0; i < N; i++)
+		X[i] = end;
+	return X;
+}
+
+void output(const int X[], const int A[][N])
+{
+	cout << "Матрица A:\n";
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout << A[i][j] << "\t";
+		cout << endl;
+	}
+	cout << "Массив X:\n";
+	for (int i = 0; i < N; i++)
+		cout << X[i] << "\t";
+}
+
 int main()
 {
 	setlocale(0, "");
-	int A[N][N]; int X[N], Y[N];
+	int A[N][N]; int X[N], c, y;
 
+	void (*pfunc) (int [N][N]);
+	do
+	{
+		cout << "Ввод матрицы:\n1 - с клавиатуры\n2 - из файла\n";
+		cin >> c;
+	} while (c != 1 && c != 2);
+
+	if (c == 1) pfunc = &readarr;
+	else pfunc = &filearr;
+
+	output(mas_x(A, X, pfunc), A);
+	_getch();
 }
