d5fcfe024d168b37a4bd30dabbc16e1898b5c199
HomoHikka <halfideas31@gmail.com>
2020-06-28 15:33:00 +0300

    Add filearr method

--- lab_7/main.cpp
+++ lab_7/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #define N 5
 
 using namespace std;
@@ -11,10 +12,26 @@ void readarr(int X[N][N])
 			cin >> X[i][j];
 }
 
+void filearr(int X[N][N])
+{
+	ifstream file ("in.txt");
+	if (file)
+	{
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				file >> X[i][j];
+	}
+	else
+	{
+		cout << "¬ходной файл отсутсвует\n";
+		exit;
+	}
+	file.close();
+}
+
 int main()
 {
 	setlocale(0, "");
 	int A[N][N]; int X[N], Y[N];
 
-	readarr(A);
 }
