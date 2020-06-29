1e0c0c9cc59b9b53d1c814ff50ebeb10db23fff4
HomoHikka <halfideas31@gmail.com>
2020-06-29 20:13:04 +0300

    Add readarr method

--- lab_9a/main.cpp
+++ lab_9a/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void readarr(int **X, int N)
+{
+	cout << "¬ведите элементы матрицы:\n";
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> X[i][j];
+}
 
 int main()
 {
 	setlocale(0, "");
+	int N, ** p;
+	cout << "¬ведите размер массива:\n";
+	cin >> N;
+	p = new int* [N];
+	for (int i = 0; i < N; i++)
+		p[i] = new int[N];
+
+	readarr(p, N);
+
+	_getch();
 }
